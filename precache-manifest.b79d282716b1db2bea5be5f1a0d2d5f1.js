5f555fc563d806ac37a405c7d16e48167a321246
Luthfulahi <luteeph@gmail.com>
2019-11-23 16:38:52 +0100

    deploy

---
+++ precache-manifest.b79d282716b1db2bea5be5f1a0d2d5f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2542a0bc60bfac4c53a4",
+    "url": "/nigerian-senators/css/app.87a9fda9.css"
+  },
+  {
+    "revision": "8e97671bf5303eac6ae392b03d9de04d",
+    "url": "/nigerian-senators/index.html"
+  },
+  {
+    "revision": "693a6f160f5f52535378",
+    "url": "/nigerian-senators/js/about.8136ed0a.js"
+  },
+  {
+    "revision": "2542a0bc60bfac4c53a4",
+    "url": "/nigerian-senators/js/app.711e2d94.js"
+  },
+  {
+    "revision": "f33f5ec75ca5a2f744e5",
+    "url": "/nigerian-senators/js/chunk-vendors.45dfe91e.js"
+  },
+  {
+    "revision": "74ca97b54115bea377d69a77fd23299d",
+    "url": "/nigerian-senators/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/nigerian-senators/robots.txt"
+  }
+]);
